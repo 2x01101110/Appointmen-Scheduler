9d234356f24844d29c1482dd3f4b5c5fcfd39840
2x01101110 <edvard.gst@outlook.com>
2020-07-25 12:30:39 +0300

    Api gateway

---
+++ src/ApiGateways/Web.Bff/Web.Bff.HttpAggregator/Services/IServicesService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Bff.HttpAggregator.Services
+{
+    public interface IServicesService
+    {
+    }
+}
